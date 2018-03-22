10d2d920158feced7630cbe3d5c8fbda9e964a86
pazuzu111 <pazuzublue111@gmail.com>
2018-03-22 06:56:35 -0400

    clean up for deploy

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 3001;
 app.use('/browse', require('./routes/browseRouter'));
 app.use('/item', require('./routes/itemRouter'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./build'));
+}
+
 app.listen(port, function () {
     console.log('Example app listening at localhost:%s', port);
 });
