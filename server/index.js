af8ca3bae46ae45edee36b703fbaa02254d9c166
pazuzu111 <pazuzublue111@gmail.com>
2018-03-22 07:09:26 -0400

    clean up for deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const path = require('path')
 
 app.use('/browse', require('./routes/browseRouter'));
 app.use('/item', require('./routes/itemRouter'));
