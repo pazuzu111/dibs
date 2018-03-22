14e3faec9f0504530a0eec7b1d382359cc081087
pazuzu111 <pazuzublue111@gmail.com>
2018-03-22 16:16:19 -0400

    clean up for deploy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('/item', require('./server/routes/itemRouter'));
 // }
 
 app.get('*', function (req, res) {
-  res.sendFile("index.html", { root: path.join(__dirname, 'build') })
+  res.sendFile("index.html", { root: path.join(__dirname, 'public') })
 });
 
 app.listen(port, function () {
