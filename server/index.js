e26e1c915dc7c3dd601e47afe5c70458826f2d74
pazuzu111 <pazuzublue111@gmail.com>
2018-03-22 07:17:16 -0400

    clean up for deploy

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, function () {
