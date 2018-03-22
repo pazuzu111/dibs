7977199f826d6ec11b77a2b0bbfa3c4aedbaab7a
pazuzu111 <pazuzublue111@gmail.com>
2018-03-22 07:37:10 -0400

    clean up for deploy

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ const path = require('path')
 app.use('/browse', require('./routes/browseRouter'));
 app.use('/item', require('./routes/itemRouter'));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('./build'));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./build'));
+}
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
