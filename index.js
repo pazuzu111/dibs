dacdb6b2568a8b532d7b2f0cb1fbabac0cb10eba
pazuzu111 <pazuzublue111@gmail.com>
2018-03-23 07:55:47 -0400

    added ping so dyno wont sleep

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 const path = require('path')
+const http = require("http");
 
 app.use('/browse', require('./server/routes/browseRouter'));
 app.use('/item', require('./server/routes/itemRouter'));
@@ -19,6 +20,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+
+setInterval(function() {
+    http.get("https://firstdibs.herokuapp.com/browse/");
+}, 1200000);
+
 app.listen(port, function () {
     console.log('Example app listening at localhost:%s', port);
 });
+
